2560221c3518380ca016e6909584c1e6f3a5612f
Jo <42.jochang@gmail.com>
2023-09-13 05:52:44 -0700

    js exercises 2-7

--- foundations/javascript/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ foundations/javascript/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(array, ...elementsToRemove) {
+	for (let i = 0; i < elementsToRemove.length; i++) {
+		array = array.filter(function(a) { return a !== elementsToRemove[i]; });
+	}
+	return array;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
--- foundations/javascript/javascript-exercises/05_sumAll/sumAll.js
+++ foundations/javascript/javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+	if (start < 0 || end < 0)
+		return "ERROR";
+	if (!(typeof start === "number" && typeof end === "number"))
+		return "ERROR";
 
+	if (start > end)
+		[start, end] = [end, start]
+	return ((end * (end + 1) / 2) - ((start- 1) * start / 2));
 };
 
 // Do not edit below this line
