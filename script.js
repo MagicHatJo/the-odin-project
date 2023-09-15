9fcf4a58741d468ba0fc8e6ebb78ea786e280b0e
Jo <42.jochang@gmail.com>
2023-09-15 11:18:11 -0700

    updated with etch-a-sketch project

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("rps-button").addEventListener("click", function() {
 	window.location.href = "./foundations/javascript/rock-paper-scissors/index.html";
 });
+
+document.getElementById("eas-button").addEventListener("click", function() {
+	window.location.href = "./foundations/etch-a-sketch/index.html";
+});
