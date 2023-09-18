379f4323e4d9486422fa1d9ef1a94ba8073b1dc1
Jo <42.jochang@gmail.com>
2023-09-18 00:59:44 -0700

    added event listeners for every calculator button

---
+++ foundations/calculator/script.js
@@ -0,0 +1,201 @@
+// Buttons
+
+// Number System
+const buttonHEX = document.getElementById("button-hex");
+buttonHEX.addEventListener("click", () => {
+	console.log("HEX clicked");
+});
+
+const buttonDEC = document.getElementById("button-dec");
+buttonDEC.addEventListener("click", () => {
+	console.log("DEC clicked");
+});
+
+const buttonOCT = document.getElementById("button-oct");
+buttonOCT.addEventListener("click", () => {
+	console.log("OCT clicked");
+});
+
+const buttonBIN = document.getElementById("button-bin");
+buttonBIN.addEventListener("click", () => {
+	console.log("BIN clicked");
+});
+
+// Bitwise
+const buttonAND = document.getElementById("button-and");
+buttonAND.addEventListener("click", () => {
+	console.log("AND clicked");
+});
+
+const buttonOR = document.getElementById("button-or");
+buttonOR.addEventListener("click", () => {
+	console.log("OR clicked");
+});
+
+const buttonNOT = document.getElementById("button-not");
+buttonNOT.addEventListener("click", () => {
+	console.log("NOT clicked");
+});
+
+const buttonNAND = document.getElementById("button-nand");
+buttonNAND.addEventListener("click", () => {
+	console.log("NAND clicked");
+});
+
+const buttonNOR = document.getElementById("button-nor");
+buttonNOR.addEventListener("click", () => {
+	console.log("NOR clicked");
+});
+
+const buttonXOR = document.getElementById("button-xor");
+buttonXOR.addEventListener("click", () => {
+	console.log("XOR clicked");
+});
+
+const buttonSHIFTLEFT = document.getElementById("button-shift-left");
+buttonSHIFTLEFT.addEventListener("click", () => {
+	console.log("Shift Left clicked");
+});
+
+const buttonSHIFTRIGHT = document.getElementById("button-shift-right");
+buttonSHIFTRIGHT.addEventListener("click", () => {
+	console.log("Shift Right clicked");
+});
+
+// Operators
+const buttonOpenParen = document.getElementById("button-open-paren");
+buttonOpenParen.addEventListener("click", () => {
+	console.log("( clicked");
+});
+
+const buttonCloseParen = document.getElementById("button-close-paren");
+buttonCloseParen.addEventListener("click", () => {
+	console.log(") clicked");
+});
+
+const buttonMod = document.getElementById("button-mod");
+buttonMod.addEventListener("click", () => {
+	console.log("% clicked");
+});
+
+const buttonDivide = document.getElementById("button-divide");
+buttonDivide.addEventListener("click", () => {
+	console.log("/ clicked");
+});
+
+const buttonTimes = document.getElementById("button-times");
+buttonTimes.addEventListener("click", () => {
+	console.log("* clicked");
+});
+
+const buttonMinus = document.getElementById("button-minus");
+buttonMinus.addEventListener("click", () => {
+	console.log("- clicked");
+});
+
+const buttonPlus = document.getElementById("button-plus");
+buttonPlus.addEventListener("click", () => {
+	console.log("+ clicked");
+});
+
+// Operands
+const button0 = document.getElementById("button-0");
+button0.addEventListener("click", () => {
+	console.log("0 clicked");
+});
+
+const button1 = document.getElementById("button-1");
+button1.addEventListener("click", () => {
+	console.log("1 clicked");
+});
+
+const button2 = document.getElementById("button-2");
+button2.addEventListener("click", () => {
+	console.log("2 clicked");
+});
+
+const button3 = document.getElementById("button-3");
+button3.addEventListener("click", () => {
+	console.log("3 clicked");
+});
+
+const button4 = document.getElementById("button-4");
+button4.addEventListener("click", () => {
+	console.log("4 clicked");
+});
+
+const button5 = document.getElementById("button-5");
+button5.addEventListener("click", () => {
+	console.log("5 clicked");
+});
+
+const button6 = document.getElementById("button-6");
+button6.addEventListener("click", () => {
+	console.log("6 clicked");
+});
+
+const button7 = document.getElementById("button-7");
+button7.addEventListener("click", () => {
+	console.log("7 clicked");
+});
+
+const button8 = document.getElementById("button-8");
+button8.addEventListener("click", () => {
+	console.log("8 clicked");
+});
+
+const button9 = document.getElementById("button-9");
+button9.addEventListener("click", () => {
+	console.log("9 clicked");
+});
+
+const buttonA = document.getElementById("button-a");
+buttonA.addEventListener("click", () => {
+	console.log("a clicked");
+});
+
+const buttonB = document.getElementById("button-b");
+buttonB.addEventListener("click", () => {
+	console.log("b clicked");
+});
+
+const buttonC = document.getElementById("button-c");
+buttonC.addEventListener("click", () => {
+	console.log("c clicked");
+});
+
+const buttonD = document.getElementById("button-d");
+buttonD.addEventListener("click", () => {
+	console.log("d clicked");
+});
+
+const buttonE = document.getElementById("button-e");
+buttonE.addEventListener("click", () => {
+	console.log("e clicked");
+});
+
+const buttonF = document.getElementById("button-f");
+buttonF.addEventListener("click", () => {
+	console.log("f clicked");
+});
+
+const buttonSign = document.getElementById("button-sign");
+buttonSign.addEventListener("click", () => {
+	console.log("sign clicked");
+});
+
+// Etc
+const buttonClear = document.getElementById("button-clear");
+buttonClear.addEventListener("click", () => {
+	console.log("Clear clicked");
+});
+
+const buttonDelete = document.getElementById("button-delete");
+buttonDelete.addEventListener("click", () => {
+	console.log("DEL clicked");
+});
+
+const buttonEquals = document.getElementById("button-equals");
+buttonEquals.addEventListener("click", () => {
+	console.log("= clicked");
+});
