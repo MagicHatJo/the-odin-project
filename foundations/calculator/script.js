77046d8b0763ba8786c31b2cef9a8d2cde4601cb
Jo <42.jochang@gmail.com>
2023-09-19 11:03:44 -0700

    operator and operand functionality

--- foundations/calculator/script.js
+++ foundations/calculator/script.js
@@ -1,201 +1,228 @@
+// Output
+const currentFormula = document.getElementById("current-formula");
+const currentOperand = document.getElementById("current-operand");
+
+const baseOut = {
+	 "2" : document.getElementById("bin-out"),
+	 "8" : document.getElementById("oct-out"),
+	"10" : document.getElementById("dec-out"),
+	"16" : document.getElementById("hex-out")
+}
+
+// History
+const calculatorHistory = document.getElementById("calculator-history");
+
+// Other
+let currentBase = 10;
+let resetOperand = true;
+const outputTokens = [];
+
+// Functions
+function updateOuts() {
+	baseOut["10"].textContent = parseInt(currentOperand.textContent, currentBase);
+	baseOut["16"].textContent = parseInt(baseOut["10"].textContent).toString(16).toUpperCase();
+	baseOut[ "8"].textContent = parseInt(baseOut["10"].textContent).toString(8);
+	baseOut[ "2"].textContent = parseInt(baseOut["10"].textContent).toString(2);
+}
+
+function updateFormula() {
+	currentFormula.textContent = "";
+	for (let token of outputTokens) {
+		if (typeof(token) === "number") {
+			token = parseInt(token).toString(currentBase);
+			if (currentBase === 16) {
+				token = token.toUpperCase();
+			}
+		}
+		currentFormula.textContent += " " + token;
+	}
+}
+
+function setPrimaryOut(base) {
+	if (["2", "8", "10", "16"].includes(base)) {
+		currentBase = base;
+		currentOperand.textContent = baseOut[base].textContent;
+	}
+}
+
+function updateKeys() {
+	for (const key in operandButtons) {
+		operandButtons[key].classList.toggle("disabled", currentBase < parseInt(key, 16) + 1);
+	}
+}
+
+function clearAll() {
+	currentFormula.textContent = "";
+	currentOperand.textContent = "0";
+	outputTokens.length = 0;
+	resetOperand = true;
+	updateOuts();
+}
+
+function deleteLast() {
+	if (currentOperand.textContent.length >= 1) {
+		currentOperand.textContent = currentOperand.textContent.slice(0, -1);
+	}
+	if (currentOperand.textContent.length === 0) {
+		currentOperand.textContent = "0";
+	}
+	updateOuts();
+}
+
+function updateOperand(num) {
+	if (currentOperand.textContent == "0") {
+		currentOperand.textContent = "";
+	}
+	currentOperand.textContent += num;
+}
+
+function loadOperand() {
+	if (outputTokens.length === 0 || ( 
+			operatorButtons.hasOwnProperty(outputTokens[outputTokens.length - 1]) &&
+			outputTokens[outputTokens.length - 1] !== ")")) {
+		outputTokens.push(parseInt(currentOperand.textContent, currentBase));
+	}
+}
+
+function loadOperator(op) {
+	if (outputTokens.length > 0 &&
+		resetOperand &&
+		!["(", ")"].includes(outputTokens[outputTokens.length - 1])) {
+		outputTokens[outputTokens.length - 1] = op;
+	}
+	else {
+		outputTokens.push(op);
+	}
+	resetOperand = true;
+}
+
 // Buttons
 
 // Number System
-const buttonHEX = document.getElementById("button-hex");
-buttonHEX.addEventListener("click", () => {
-	console.log("HEX clicked");
-});
+const baseButtons = {
+	 "2" : document.getElementById("button-bin"),
+	 "8" : document.getElementById("button-oct"),
+	"10" : document.getElementById("button-dec"),
+	"16" : document.getElementById("button-hex")
+}
 
-const buttonDEC = document.getElementById("button-dec");
-buttonDEC.addEventListener("click", () => {
-	console.log("DEC clicked");
-});
-
-const buttonOCT = document.getElementById("button-oct");
-buttonOCT.addEventListener("click", () => {
-	console.log("OCT clicked");
-});
-
-const buttonBIN = document.getElementById("button-bin");
-buttonBIN.addEventListener("click", () => {
-	console.log("BIN clicked");
-});
-
-// Bitwise
-const buttonAND = document.getElementById("button-and");
-buttonAND.addEventListener("click", () => {
-	console.log("AND clicked");
-});
-
-const buttonOR = document.getElementById("button-or");
-buttonOR.addEventListener("click", () => {
-	console.log("OR clicked");
-});
-
-const buttonNOT = document.getElementById("button-not");
-buttonNOT.addEventListener("click", () => {
-	console.log("NOT clicked");
-});
-
-const buttonNAND = document.getElementById("button-nand");
-buttonNAND.addEventListener("click", () => {
-	console.log("NAND clicked");
-});
-
-const buttonNOR = document.getElementById("button-nor");
-buttonNOR.addEventListener("click", () => {
-	console.log("NOR clicked");
-});
-
-const buttonXOR = document.getElementById("button-xor");
-buttonXOR.addEventListener("click", () => {
-	console.log("XOR clicked");
-});
-
-const buttonSHIFTLEFT = document.getElementById("button-shift-left");
-buttonSHIFTLEFT.addEventListener("click", () => {
-	console.log("Shift Left clicked");
-});
-
-const buttonSHIFTRIGHT = document.getElementById("button-shift-right");
-buttonSHIFTRIGHT.addEventListener("click", () => {
-	console.log("Shift Right clicked");
-});
+for (const key in baseButtons) {
+	baseButtons[key].addEventListener("click", () => {
+		setPrimaryOut(key);
+		updateKeys();
+		updateFormula();
+	});
+}
 
 // Operators
-const buttonOpenParen = document.getElementById("button-open-paren");
-buttonOpenParen.addEventListener("click", () => {
-	console.log("( clicked");
+operatorButtons = {
+	"AND"  : document.getElementById("button-and"),
+	"OR"   : document.getElementById("button-or"),
+	"NOT"  : document.getElementById("button-not"),
+	"NAND" : document.getElementById("button-nand"),
+	"NOR"  : document.getElementById("button-nor"),
+	"XOR"  : document.getElementById("button-xor"),
+	"«"    : document.getElementById("button-shift-left"),
+	"»"    : document.getElementById("button-shift-right"),
+	"%"    : document.getElementById("button-mod"),
+	"÷"    : document.getElementById("button-divide"),
+	"x"    : document.getElementById("button-times"),
+	"-"    : document.getElementById("button-minus"),
+	"+"    : document.getElementById("button-plus")
+}
+
+for (const key in operatorButtons) {
+	operatorButtons[key].addEventListener("click", () => {
+		if (!resetOperand) {
+			loadOperand();
+		}
+		loadOperator(key);
+		updateFormula();
+	});
+}
+
+let parenCounter = 0;
+
+operatorButtons["("] = document.getElementById("button-open-paren");
+operatorButtons["("].addEventListener("click", () => {
+	if (!resetOperand) {
+		loadOperand();
+		loadOperator("x");
+	}
+	outputTokens.push("(");
+	updateFormula();
+	parenCounter++;
 });
 
-const buttonCloseParen = document.getElementById("button-close-paren");
-buttonCloseParen.addEventListener("click", () => {
-	console.log(") clicked");
-});
-
-const buttonMod = document.getElementById("button-mod");
-buttonMod.addEventListener("click", () => {
-	console.log("% clicked");
-});
-
-const buttonDivide = document.getElementById("button-divide");
-buttonDivide.addEventListener("click", () => {
-	console.log("/ clicked");
-});
-
-const buttonTimes = document.getElementById("button-times");
-buttonTimes.addEventListener("click", () => {
-	console.log("* clicked");
-});
-
-const buttonMinus = document.getElementById("button-minus");
-buttonMinus.addEventListener("click", () => {
-	console.log("- clicked");
-});
-
-const buttonPlus = document.getElementById("button-plus");
-buttonPlus.addEventListener("click", () => {
-	console.log("+ clicked");
+operatorButtons[")"] = document.getElementById("button-close-paren");
+operatorButtons[")"].addEventListener("click", () => {
+	if (parenCounter > 0) {
+		loadOperand();
+		outputTokens.push(")");
+		resetOperand = true;
+		updateFormula();
+		parenCounter--;
+	}
 });
 
 // Operands
-const button0 = document.getElementById("button-0");
-button0.addEventListener("click", () => {
-	console.log("0 clicked");
-});
+const operandButtons = {
+	"0" : document.getElementById("button-0"),
+	"1" : document.getElementById("button-1"),
+	"2" : document.getElementById("button-2"),
+	"3" : document.getElementById("button-3"),
+	"4" : document.getElementById("button-4"),
+	"5" : document.getElementById("button-5"),
+	"6" : document.getElementById("button-6"),
+	"7" : document.getElementById("button-7"),
+	"8" : document.getElementById("button-8"),
+	"9" : document.getElementById("button-9"),
+	"A" : document.getElementById("button-a"),
+	"B" : document.getElementById("button-b"),
+	"C" : document.getElementById("button-c"),
+	"D" : document.getElementById("button-d"),
+	"E" : document.getElementById("button-e"),
+	"F" : document.getElementById("button-f"),
+}
 
-const button1 = document.getElementById("button-1");
-button1.addEventListener("click", () => {
-	console.log("1 clicked");
-});
-
-const button2 = document.getElementById("button-2");
-button2.addEventListener("click", () => {
-	console.log("2 clicked");
-});
-
-const button3 = document.getElementById("button-3");
-button3.addEventListener("click", () => {
-	console.log("3 clicked");
-});
-
-const button4 = document.getElementById("button-4");
-button4.addEventListener("click", () => {
-	console.log("4 clicked");
-});
-
-const button5 = document.getElementById("button-5");
-button5.addEventListener("click", () => {
-	console.log("5 clicked");
-});
-
-const button6 = document.getElementById("button-6");
-button6.addEventListener("click", () => {
-	console.log("6 clicked");
-});
-
-const button7 = document.getElementById("button-7");
-button7.addEventListener("click", () => {
-	console.log("7 clicked");
-});
-
-const button8 = document.getElementById("button-8");
-button8.addEventListener("click", () => {
-	console.log("8 clicked");
-});
-
-const button9 = document.getElementById("button-9");
-button9.addEventListener("click", () => {
-	console.log("9 clicked");
-});
-
-const buttonA = document.getElementById("button-a");
-buttonA.addEventListener("click", () => {
-	console.log("a clicked");
-});
-
-const buttonB = document.getElementById("button-b");
-buttonB.addEventListener("click", () => {
-	console.log("b clicked");
-});
-
-const buttonC = document.getElementById("button-c");
-buttonC.addEventListener("click", () => {
-	console.log("c clicked");
-});
-
-const buttonD = document.getElementById("button-d");
-buttonD.addEventListener("click", () => {
-	console.log("d clicked");
-});
-
-const buttonE = document.getElementById("button-e");
-buttonE.addEventListener("click", () => {
-	console.log("e clicked");
-});
-
-const buttonF = document.getElementById("button-f");
-buttonF.addEventListener("click", () => {
-	console.log("f clicked");
-});
+for (const key in operandButtons) {
+	operandButtons[key].addEventListener("click", () => {
+		if (!operandButtons[key].classList.contains("disabled")) {
+			if (resetOperand) {
+				currentOperand.textContent = "";
+				resetOperand = false;
+			}
+			updateOperand(key);
+			updateOuts();
+		}
+	});
+}
 
 const buttonSign = document.getElementById("button-sign");
-buttonSign.addEventListener("click", () => {
-	console.log("sign clicked");
-});
+buttonSign.classList.add("disabled");
+// buttonSign.addEventListener("click", () => {
+//
+// });
 
 // Etc
 const buttonClear = document.getElementById("button-clear");
 buttonClear.addEventListener("click", () => {
-	console.log("Clear clicked");
+	clearAll();
 });
 
 const buttonDelete = document.getElementById("button-delete");
 buttonDelete.addEventListener("click", () => {
-	console.log("DEL clicked");
+	deleteLast();
 });
 
 const buttonEquals = document.getElementById("button-equals");
 buttonEquals.addEventListener("click", () => {
 	console.log("= clicked");
+	//shunting yard
+	//calculate rpn
+	//push to current operand
+	//push to history
 });
+
+// Start
+updateKeys(currentBase);
