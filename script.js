9e181417195c1abeb9f8e04a3334b986d181a3b5
Jo <42.jochang@gmail.com>
2023-09-27 09:41:30 -0700

    added odin-recipes

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.getElementById("recipe-button").addEventListener("click", function() {
+	window.location.href = "./foundations/odin-recipes/index.html";
+});
+
 document.getElementById("rps-button").addEventListener("click", function() {
 	window.location.href = "./foundations/javascript/rock-paper-scissors/index.html";
 });
